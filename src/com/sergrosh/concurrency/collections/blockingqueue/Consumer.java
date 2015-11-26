9f156296224c00c8702a2f7d566d8def9ab4d372
Sergii Roshchupkin <sergey.roshchupkin@gmail.com>
2015-11-26 08:26:58 +0100

    added some examples

---
+++ src/com/sergrosh/concurrency/collections/blockingqueue/Consumer.java
@@ -0,0 +1,29 @@
+package com.sergrosh.concurrency.collections.blockingqueue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Sergey on 11/25/2015.
+ */
+public class Consumer implements Runnable{
+
+    protected BlockingQueue queue = null;
+
+    public Consumer(BlockingQueue  queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(2000);
+            System.out.println("take: "+queue.take());
+            Thread.sleep(2000);
+
+            System.out.println("take: "+queue.take());
+            System.out.println("take: "+queue.poll(5, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/sergrosh/concurrency/collections/blockingqueue/Producer.java
@@ -0,0 +1,32 @@
+package com.sergrosh.concurrency.collections.blockingqueue;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by Sergey on 11/25/2015.
+ */
+public class Producer implements Runnable{
+
+    protected BlockingQueue queue = null;
+
+    public Producer(BlockingQueue  queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            System.out.println("put 1");
+            queue.put("1");
+
+            Thread.sleep(3000);
+            System.out.println("put 2");
+            queue.put("2");
+
+            Thread.sleep(3000);
+            queue.put("3");
+            System.out.println("put 3");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
